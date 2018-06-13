0df5fc0ad583d6dbf7e13ba76cc5ab27955d66a1
michalklawik <WejhiT261f>
2018-06-13 20:01:40 +0200

    styiling window

--- src/main/java/kata/Okno.java
+++ src/main/java/kata/Okno.java
@@ -1,9 +1,21 @@
 package kata;
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 public class Okno extends Application {
@@ -14,18 +26,50 @@ public class Okno extends Application {
 
     @Override
     public void start(Stage primaryStage) {
-        StackPane stackPane = new StackPane();
-        Button button = new Button("Przycisk");
-        stackPane.getChildren().add(button);
+        primaryStage.setTitle("JavaFX Welcome");
 
-        Scene scene = new Scene(stackPane, 400, 600);
+        GridPane gridPane = new GridPane();
+        gridPane.setAlignment(Pos.CENTER);
+        gridPane.setHgap(10);
+        gridPane.setVgap(10);
+        gridPane.setPadding(new Insets(25, 25, 25, 25));
 
+        Text sceneTitle = new Text("Welcome");
+        sceneTitle.setId("welcome-text");
+        gridPane.add(sceneTitle, 0, 0, 2, 1);
+
+        Label userName = new Label("User Name:");
+        gridPane.add(userName, 0, 1);
+
+        TextField userTextField = new TextField();
+        gridPane.add(userTextField, 1, 1);
+
+        Label pw = new Label("Password:");
+        gridPane.add(pw, 0, 2);
+
+        PasswordField pwTextField = new PasswordField();
+        gridPane.add(pwTextField, 1, 2);
+
+        Button btn = new Button("Sign in");
+        HBox hbBtn = new HBox(10);
+        hbBtn.setAlignment(Pos.BOTTOM_RIGHT);
+        hbBtn.getChildren().add(btn);
+        gridPane.add(hbBtn, 1, 4);
+
+        final Text actionTarget = new Text();
+        actionTarget.setId("actiontarget");
+        gridPane.add(actionTarget, 1, 6);
+
+        btn.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                actionTarget.setText("Sign in button pressed");
+            }
+        });
+
+        Scene scene = new Scene(gridPane, 400, 275);
         primaryStage.setScene(scene);
-
-        primaryStage.setWidth(500);
-        primaryStage.setHeight(500);
-        primaryStage.setOpacity(0.8);
-        primaryStage.setTitle("Pierwsze okno");
+        scene.getStylesheets().add(Okno.class.getResource("style.css").toExternalForm());
         primaryStage.show();
     }
 }
