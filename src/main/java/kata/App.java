9208b7253d514b900bdef58dd2e358d116d749a8
michalklawik <WejhiT261f>
2018-06-18 20:04:33 +0200

    Small refactor

--- src/main/java/kata/App.java
+++ src/main/java/kata/App.java
@@ -7,10 +7,6 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 
-/**
- * Hello world!
- *
- */
 public class App 
 {
     public static void main( String[] args )
